7c3b9322aa2ffc96ecf356e7220661389e5b7f5e
yuta001026 <97606911+yuta001026@users.noreply.github.com>
2022-04-29 12:28:04 +0900

    commit2

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true length: { maximum: 6 }
+         validates :email, presence: true
+         validates :password
+
+end
