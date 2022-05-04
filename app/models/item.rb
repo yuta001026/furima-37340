93c1bca91b0a34a63221273996fb58690cc9ccde
yuta001026 <97606911+yuta001026@users.noreply.github.com>
2022-05-04 17:40:57 +0900

    商品出品機能2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :region
+  belongs_to :category
+  belongs_to :shipping_day
+  belongs_to :state
+  belongs_to :delivery_fee
   belongs_to :user
   has_one_attached :image
+  validates :name,    presence: true
+  validates :explain, presence: true
+  validates :price,   presence: true
+  validates :user,    presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Region < ActiveHash::Base
   self.data = [
    { id: 1, name: '--' },
    { id: 2, name: '北海道' },
@@ -49,4 +49,8 @@ class Genre < ActiveHash::Base
    { id: 47, name: '鹿児島' },
    { id: 48, name: '沖縄' },
   ]
+
+  include ActiveHash::Associations
+  has_many :items
+  
   end
